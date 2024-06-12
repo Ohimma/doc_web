c347ded57473b500a8864a95235524d0f0271ff8
Teng XunTao <tengxuntao@itiger.com>
2024-06-12 17:05:42 +0800

    add 2024-06-12

--- push.sh
+++ push.sh
@@ -2,13 +2,14 @@
 
 # git remote add github git@github.com:Ohimma/doc_web.git
 # git remote add gitee  git@gitee.com:ohimma/doc_web.git
+
 mes=$1
 
-if [ -z $1 ];then
+if [[ -z $1 ]];then
    echo "请输入commit提交信息...."
    exit
 else
-   echo "git commit -m \"$mes\""
+   echo "\033[1;34mgit commit -m $mes\033[0m"
    git add --all .
    git commit -m "$mes"
    git push github master
